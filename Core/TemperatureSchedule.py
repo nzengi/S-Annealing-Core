d8f9c77da29b4e51c52e5232eb28c40958f92c24
nzengi <zenginureddin1@gmail.com>
2024-09-01 00:33:37 +0300

    Create TemperatureSchedule.py

---
+++ Core/TemperatureSchedule.py
@@ -0,0 +1,20 @@
+class TemperatureSchedule:
+    def __init__(self, initial_temp):
+        self.initial_temp = initial_temp
+
+    def initial_temperature(self):
+        return self.initial_temp
+
+    def update_temperature(self, temp, energy_diff, stagnation_counter):
+        if stagnation_counter > self.stagnation_threshold():
+            return temp * 0.9
+        elif energy_diff > significant_energy_drop():
+            return temp * 0.99
+        else:
+            return temp * 0.95
+
+    def stagnation_threshold(self):
+        return 100  # Example value
+
+    def significant_energy_drop(self):
+        return 50  # Example value
