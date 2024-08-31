faf1cf066ed3c15c60c1ef452b78853291bff5fd
nzengi <zenginureddin1@gmail.com>
2024-09-01 00:37:14 +0300

    Create Config.py

---
+++ Configuration/Config.py
@@ -0,0 +1,9 @@
+import json
+
+class Config:
+    def __init__(self, config_file='config.json'):
+        with open(config_file, 'r') as f:
+            self.config = json.load(f)
+
+    def get(self, key, default=None):
+        return self.config.get(key, default)
